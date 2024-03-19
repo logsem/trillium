46397c6d0d9e506e8c59442442efc75a15b56616
Léo Stefanesco <leo.lveb@gmail.com>
2024-03-19 16:52:44 +0100

    Rewrite the model in the new setting

--- fairneris/examples/retransmit_model.v
+++ fairneris/examples/retransmit_model.v
@@ -1,7 +1,7 @@
 From trillium.prelude Require Export finitary quantifiers sigma classical_instances.
 From Paco Require Import paco1 paco2 pacotac.
 From fairneris Require Export trace_utils fairness.
-From fairneris.aneris_lang Require Import ast network.
+From fairneris.aneris_lang Require Import ast network lang.
 From fairneris Require Export trace_utils ltl_lite.
 
 Import derived_laws_later.bi.
@@ -15,45 +15,27 @@ Lemma suffix_trans {A} (l1 l2 l3 : list A) :
 Proof. intros [l1' ->] [l2' ->]. by do 2 apply suffix_app_r. Qed.
 
 (** The retransmit model states *)
-Inductive retransmit_base_state :=
+Inductive retransmit_state :=
 | Start
 | Received.
-
-Definition retransmit_state : Set :=
-  retransmit_base_state * message_multi_soup * gmap socket_address (list message).
 
 #[global] Instance simple_state_eqdec : EqDecision retransmit_state.
 Proof. intros ??. apply make_decision. Qed.
 #[global] Instance simple_state_inhabited : Inhabited retransmit_state.
-Proof. exact (populate (Start,∅,∅)). Qed.
+Proof. exact (populate Start). Qed.
 
-Inductive retransmit_node_role := Arole | Brole.
-Definition retransmit_network_role : Set := unit.
-Definition retransmit_role : Set :=
-  retransmit_node_role + retransmit_network_role.
+Inductive retransmit_role := Arole | Brole.
 
 Definition retransmit_node_action : Set := option message.
 Definition retransmit_network_action : Set := option message.
 Definition retransmit_action : Set :=
   retransmit_node_action + retransmit_network_action.
 
-Definition retransmit_node_label : Set :=
-  retransmit_node_role * retransmit_node_action.
-Definition retransmit_network_label : Set :=
-  retransmit_network_role * retransmit_network_action.
-Definition retransmit_label : Set :=
-  retransmit_node_label + retransmit_network_label.
-
-Definition label_role (l : retransmit_label) : retransmit_role :=
-  sum_map fst fst l.
-Definition label_action (l : retransmit_label) : retransmit_action :=
-  sum_map snd snd l.
-
-#[global] Instance retransmit_node_role_eqdec : EqDecision retransmit_node_role.
+#[global] Instance retransmit_role_eqdec : EqDecision retransmit_role.
 Proof. intros ??. apply make_decision. Qed.
-#[global] Instance retransmit_node_role_inhabited : Inhabited retransmit_node_role.
+#[global] Instance retransmit_role_inhabited : Inhabited retransmit_role.
 Proof. exact (populate (Arole)). Qed.
-#[global] Instance retransmit_node_role_countable : Countable retransmit_node_role.
+#[global] Instance retransmit_role_countable : Countable retransmit_role.
 Proof.
   refine ({|
              encode s := match s with
@@ -69,86 +51,38 @@ Proof.
   by intros [|].
 Qed.
 
-#[global] Instance retransmit_role_eqdec : EqDecision retransmit_role.
-Proof. intros ??. apply make_decision. Qed.
-#[global] Instance retransmit_role_inhabited : Inhabited retransmit_role.
-Proof. exact (populate (inl Arole)). Qed.
-#[global] Instance retransmit_role_countable : Countable retransmit_role.
-Proof.
-  refine ({|
-             encode s := match s with
-                         | inl Arole => 1
-                         | inl Brole => 2
-                         | inr () => 3
-                         end;
-             decode n := match n with
-                         | 1 => Some $ inl Arole
-                         | 2 => Some $ inl Brole
-                         | 3 => Some $ inr ()
-                         | _ => None
-                         end;
-         |})%positive.
-  by intros [[]|[]].
-Qed.
-
 Definition saA : socket_address := SocketAddressInet "0.0.0.0" 80.
 Definition saB : socket_address := SocketAddressInet "0.0.0.1" 80.
 Definition mAB : message := mkMessage saA saB "Hello".
 
-Inductive retransmit_trans : retransmit_state → retransmit_label → retransmit_state → Prop :=
-| A_Send st ms bs :
-  retransmit_trans (st, ms, bs)
-                   (inl $ (Arole, Some mAB))
-                   (st, ms ⊎ {[+ mAB +]}, bs)
-| N_Duplicate st ms bs msg :
-  msg ∈ ms →
-  retransmit_trans (st, ms, bs)
-                   (inr ((),None))
-                   (st, ms ⊎ {[+ msg +]}, bs)
-| N_Drop st ms bs msg :
-  msg ∈ ms →
-  retransmit_trans (st, ms, bs)
-                   (inr ((), None))
-                   (st, ms ∖ {[+ msg +]}, bs)
-| N_Deliver st ms ms' bs msg :
-  msg ∈ ms →
-  bs !!! m_destination msg = ms' →
-  retransmit_trans (st, ms, bs)
-                   (inr ((), Some msg))
-                   (st, ms ∖ {[+ msg +]}, <[m_destination msg := msg::ms']>bs)
-| B_RecvFail ms bs :
-  bs !!! saB = [] →
-  retransmit_trans (Start, ms, bs)
-                   (inl $ (Brole, None))
-                   (Start, ms, bs)
-| B_RecvSucc ms bs msg ms' :
-  bs !!! saB = ms'++[msg] →
-  retransmit_trans (Start, ms, bs)
-                   (inl $ (Brole, None))
-                   (Received, ms, <[saB := ms']>bs).
+Notation retransmit_label := (retransmit_role * option aneris_action)%type.
 
-Notation mtrace := (trace retransmit_state retransmit_label).
+Inductive retransmit_trans :
+  retransmit_state → retransmit_role * option aneris_action → retransmit_state → Prop :=
+| A_Send st :
+  retransmit_trans st (Arole, Some (Send mAB)) st
+| B_RecvFail :
+  retransmit_trans Start (Brole, Some (Recv None)) Start
+| B_RecvSucc msg :
+  retransmit_trans Start (Brole, Some (Recv (Some msg))) Received
+.
+Notation mtrace := (trace retransmit_state (retransmit_role * option aneris_action)).
 
-Definition retransmit_live_roles (s : retransmit_state) : gset retransmit_node_role :=
+Definition retransmit_live_roles (s : retransmit_state) : gset retransmit_role :=
   {[Arole]} ∪
-  (match s.1.1 with Start => {[Brole]} | _ => ∅ end).
+  (match s with Start => {[Brole]} | _ => ∅ end).
 
-Definition retransmit_role_enabled_model (ρ : retransmit_node_role) (s : retransmit_state) : Prop :=
+Definition retransmit_role_enabled_model (ρ : retransmit_role) (s : retransmit_state) : Prop :=
   ρ ∈ retransmit_live_roles s.
 
 (* TODO: This is likely not needed. *)
 Lemma retransmit_live_spec_holds s ρ α s' :
-  retransmit_trans s (inl (ρ,α)) s' → ρ ∈ retransmit_live_roles s.
+  retransmit_trans s (ρ,α) s' → ρ ∈ retransmit_live_roles s.
 Proof. inversion 1; set_solver. Qed.
 
-Definition send_filter msg : retransmit_state → option retransmit_label → Prop :=
-  λ _ l, option_map label_action l = Some $ inl $ Some msg.
+Definition send_filter msg : retransmit_state → retransmit_label → Prop :=
+  λ _ l, snd l = Some $ Send msg.
 Instance send_filter_decision msg st l : Decision (send_filter msg st l).
-Proof. apply make_decision. Qed.
-
-Definition deliver_filter msg : retransmit_state → option retransmit_label → Prop :=
-  λ _ l, l = Some $ inr ((),Some msg).
-Instance deliver_filter_decision msg st l : Decision (deliver_filter msg st l).
 Proof. apply make_decision. Qed.
 
 Definition retransmit_fair_network_delivery msg : mtrace → Prop :=
