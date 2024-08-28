8ac6b93ea50c00077d47584a2924e86ba698db3f
fresheed <ok.john.rus@gmail.com>
2024-08-28 15:24:15 +0200

    moved MU__r to separate file

---
+++ fairis/heap_lang/examples/even_odd/mu_role.v
@@ -0,0 +1,38 @@
+From iris.proofmode Require Import tactics.
+From trillium.fairness Require Import action_model resources fuel.
+From trillium.fairness.heap_lang Require Import notation iris_inst.
+
+
+Section MuRole.
+  Context {AM1 AM2: ActionModel}.
+  Let PM := ProdAM AM1 AM2.
+  Context {PROD_LR: AM_strong_lr PM}. 
+  Let M := AM2FM PM PROD_LR.
+
+  Context {LM: LiveModel heap_lang M}.
+
+  Context `{!heapGS Σ AM1 AM2}. 
+            
+  Definition MU__r ρ E P: iProp Σ :=  
+    ∀ τ f R, τ ↦M ({[ ρ := f ]} ∪ (S <$> R)) ∗ ⌜ ρ ∉ dom R ⌝ -∗
+              MU E τ (τ ↦M ({[ ρ := lm_flm LM ]} ∪ R) ∗ P) (LM := LM).
+
+  Lemma MU__r_wand E ρ (P Q: iProp Σ):
+    (P -∗ Q) -∗ MU__r ρ E P -∗ MU__r ρ E Q.
+  Proof.
+    iIntros "HPQ HMU". rewrite /MU__r. iIntros "**".
+    iSpecialize ("HMU" with "[$]"). 
+    iApply (MU_wand with "[HPQ] [$]"). iFrame.
+    iIntros "[??]". iFrame. by iApply "HPQ". 
+  Qed.
+
+  Lemma MU__r_mask_weaken E1 E2 ρ (P: iProp Σ)
+    (SUB: E1 ⊆ E2):
+    MU__r ρ E1 P -∗ MU__r ρ E2 P.
+  Proof.
+    iIntros "MU". rewrite /MU__r. iIntros "**".
+    iApply MU_mask_weaken; eauto.
+    by iApply "MU".
+  Qed.
+
+End MuRole.
--- fairis/heap_lang/examples/even_odd/thread_progs.v
+++ fairis/heap_lang/examples/even_odd/thread_progs.v
@@ -139,32 +139,6 @@ Section StResImpl.
 End StResImpl. 
 
 
-Definition MU__r `{LM: LiveModel heap_lang M} `{!heapGS Σ LM} ρ E P: iProp Σ :=  
-  ∀ τ f R, τ ↦M ({[ ρ := f ]} ∪ (S <$> R)) ∗ ⌜ ρ ∉ dom R ⌝ -∗
-          MU E τ (τ ↦M ({[ ρ := lm_flm LM ]} ∪ R) ∗ P).
-
-
-Lemma MU__r_wand `{LM: LiveModel heap_lang M} `{!heapGS Σ LM} E ρ (P Q : iProp Σ) :
-  (P -∗ Q) -∗ MU__r ρ E P -∗ MU__r ρ E Q.
-Proof.
-  iIntros "HPQ HMU". rewrite /MU__r. iIntros "**".
-  iSpecialize ("HMU" with "[$]"). 
-  iApply (MU_wand with "[HPQ] [$]"). iFrame.
-  iIntros "[??]". iFrame. by iApply "HPQ". 
-Qed.
-
-
-Lemma MU__r_mask_weaken `{LM: LiveModel heap_lang M} `{!heapGS Σ LM}
-  E1 E2 ρ (P: iProp Σ)
-  (SUB: E1 ⊆ E2):
-  MU__r ρ E1 P -∗ MU__r ρ E2 P.
-Proof.
-  iIntros "MU". rewrite /MU__r. iIntros "**".
-  iApply MU_mask_weaken; eauto.
-  by iApply "MU".
-Qed.
-
-
 Section ProofsGen.  
   Context `{LM__p: LiveModel heap_lang M__p}.
   Context `{!heapGS Σ LM__p}.
